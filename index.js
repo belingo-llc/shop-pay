9f9a464bea3b10367bd1fbc13845da9624d0b807
Savoskin Andrey <a.savoskin@belingo.ru>
2020-02-05 20:51:03 +0300

    moysklad v4.0.103

--- index.js
+++ index.js
@@ -980,8 +980,7 @@ new CronJob('*/1 * * * *', () => {
             // change sum payment
             axios.put('https://online.moysklad.ru/api/remap/1.1/entity/paymentin/'+response.data.id, 
               {
-                "sum": ms_sumOrder,
-                "vatSum": ms_sumOrder
+                "sum": ms_sumOrder
               }, {
               headers: headers,
               auth: {username: ms_login,password: ms_pass}
