0089153bc73a504283b7595a0232306823333ba6
Savoskin Andrey <a.savoskin@belingo.ru>
2020-02-06 04:04:16 +0300

    moysklad v4.0.141

--- index.js
+++ index.js
@@ -965,7 +965,7 @@ new CronJob('*/1 * * * *', () => {
                           "assortment": {
                             "meta": {
                               "href": response.data.rows[0].meta.href,
-                              "metadataHref"  => "https://online.moysklad.ru/api/remap/1.1/entity/product/metadata",
+                              "metadataHref": "https://online.moysklad.ru/api/remap/1.1/entity/product/metadata",
                               "type": "product",
                               "mediaType": "application/json"
                             }
