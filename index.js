62b1e3f083028d5907de6352de18c2c08aea254d
Savoskin Andrey <a.savoskin@belingo.ru>
2020-02-11 10:08:49 +0300

    moysklad 4.2.12

--- index.js
+++ index.js
@@ -1296,6 +1296,17 @@ new CronJob('*/1 * * * *', () => {
             const ms_login = 'Admin@9645054848';
             const ms_pass = 'marmar3587133mar';
             var ms_telephone = shop[x].telephone;
+            var ms_fio = shop[x].fio;
+            var ms_nik = shop[x].nik;
+            var ms_numOrder = shop[x].numOrder;
+            var ms_idProduct = shop[x].idProduct;
+            var ms_purchase = shop[x].purchase;
+            var ms_delivery = shop[x].delivery;
+            var ms_delivery_address = shop[x].deliveryAddress;
+            var ms_street = shop[x].street;
+            var ms_home = shop[x].home;
+            var ms_room = shop[x].room;
+            var ms_sumOrder = shop[x].sumOrder;
 
             //search counterparty
             axios.get(
@@ -1306,32 +1317,10 @@ new CronJob('*/1 * * * *', () => {
             }).then(function(response) {
               if(response.data.rows.length > 0) {
                 var counterparty = response.data.rows[0].meta.href;
-                var ms_fio = shop[x].fio;
-                var ms_nik = shop[x].nik;
-                var ms_numOrder = shop[x].numOrder;
-                var ms_idProduct = shop[x].idProduct;
-                var ms_purchase = shop[x].purchase;
-                var ms_delivery = shop[x].delivery;
-                var ms_delivery_address = shop[x].deliveryAddress;
-                var ms_street = shop[x].street;
-                var ms_home = shop[x].home;
-                var ms_room = shop[x].room;
-                var ms_sumOrder = shop[x].sumOrder;
                 console.log('Найден контрагент '+counterparty);
                 create_ms_order(ms_sumOrder, ms_street, ms_home, ms_room, ms_purchase, ms_idProduct, ms_numOrder, counterparty, headers, ms_login, ms_pass, ms_delivery, ms_delivery_address);
               }else{
                 console.log('Контрагент не найден. Будет создан новый!');
-                var ms_fio = shop[x].fio;
-                var ms_nik = shop[x].nik;
-                var ms_numOrder = shop[x].numOrder;
-                var ms_idProduct = shop[x].idProduct;
-                var ms_purchase = shop[x].purchase;
-                var ms_delivery = shop[x].delivery;
-                var ms_delivery_address = shop[x].deliveryAddress;
-                var ms_street = shop[x].street;
-                var ms_home = shop[x].home;
-                var ms_room = shop[x].room;
-                var ms_sumOrder = shop[x].sumOrder;
                 // if counterparty not exists
                 var createCounterPartyUrl = 'https://online.moysklad.ru/api/remap/1.1/entity/counterparty';
                 var data = {
